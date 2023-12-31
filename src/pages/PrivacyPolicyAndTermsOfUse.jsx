8faab8df9f6ef0ea6a825c508fa44f12721a8fea
asadullah055 <asadullahahmed01@gmail.com>
2024-01-01 00:17:38 +0600

    first commit

---
+++ src/pages/PrivacyPolicyAndTermsOfUse.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivacyPolicyAndTermsOfUse = () => {
+    return (
+        <div>
+            <h1>This our term and policy</h1>
+        </div>
+    );
+};
+
+export default PrivacyPolicyAndTermsOfUse;
